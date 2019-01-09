23e220cd43f97e9f0404e106d44787c15e889b9f
emil323 <emilkalst@gmail.com>
2019-01-09 20:19:18 +0100

    test1

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 1337;
+
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(80, function () {
+var server = app.listen(port, function () {
+    
    var host = server.address().address
    var port = server.address().port
    
