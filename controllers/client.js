59695732f88aea61a3e823868435775360fe86e5
emil323 <emilkalst@gmail.com>
2019-02-12 19:29:06 +0100

    Updated queries for is_deleted

--- controllers/client.js
+++ controllers/client.js
@@ -16,7 +16,8 @@ exports.get = (req,res) => {
     const query = `
         SELECT name
         FROM Client
-        WHERE id = $1
+        WHERE is_deleted IS FALSE
+        AND id = $1
     `
 
     db.query(query,[clientID],(err,queryRes) => {
@@ -37,19 +38,10 @@ exports.files = (req, res) => {
     const clientID = req.params.client_id
 
     const query = `
-        SELECT id,client_id,parent_id,name,ref, type,last_changed, is_directory,
-            CASE
-            WHEN parent_id IS NULL THEN TRUE
-            ELSE FALSE
-            END AS is_root
-        FROM
-            (SELECT id, client_id, parent_id, name, null AS ref, 'folder' AS type, last_changed, true AS is_directory
-            FROM Folder
-            UNION
-            SELECT File.id, Folder.client_id, File.folder_id, File.name, File.ref, File.type, File.last_changed, false AS is_directory
-            FROM File, Folder
-            WHERE Folder.id = File.folder_id) AS U
-        WHERE client_id = $1
+        SELECT id,client_id,parent_id,name,ref, type,last_changed, is_deleted,is_directory, is_root
+        FROM FilesAndFolders
+        WHERE is_deleted IS FALSE
+        AND client_id = $1
     `
 
     db.query(query,[clientID],(err,queryRes) => {
